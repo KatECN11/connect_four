83ae1dabb0d50dce0e94babe8d808f5c88360d97
Kathleen Yruegas <kathleenecn@gmail.com>
2018-07-14 15:14:22 -0600

    Adjusts methods to account for columns

--- lib/board.rb
+++ lib/board.rb
@@ -13,12 +13,24 @@ class Board
                 :G => [".", ".", ".", ".", ".", "."]
             }
   end
-  #
-  # def print_board
-  #   printed_rows = @rows.values.map do |spots|
-  #                   spots.join
-  #                  end
-  #   @column_names.join + "\n" + printed_rows.join("\n")
-  # end
+
+  def print_board
+    row_1 = ""
+    row_2 = ""
+    row_3 = ""
+    row_4 = ""
+    row_5 = ""
+    row_6 = ""
+    @columns.values.each do |spots|
+      row_1 += spots[0]
+      row_2 += spots[1]
+      row_3 += spots[2]
+      row_4 += spots[3]
+      row_5 += spots[4]
+      row_6 += spots[5]
+    end
+    printed_board = [@column_names.join, row_1, row_2, row_3, row_4, row_5, row_6]
+    return printed_board.join("\n")
+  end
 
 end
