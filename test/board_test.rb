19ef61acb5eac8ad5d1e2cb39afe0dc635f72631
Kathleen Yruegas <kathleenecn@gmail.com>
2018-07-14 12:17:32 -0600

    Adds board class and first test for existence

---
+++ test/board_test.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/piece'
+require './lib/board'
+
+class BoardTest < Minitest::Test
+
+  def test_it_exists
+    board = Board.new
+
+    assert_instance_of Board, board
+  end 
+
+end
