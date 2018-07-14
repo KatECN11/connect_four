b488f678d35c891a44304c1ca0ed09471767548b
Kathleen Yruegas <kathleenecn@gmail.com>
2018-07-14 13:02:48 -0600

    Adds method of printing board

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,22 @@
 class Board
+  attr_reader :column_names, :rows
 
   def initialize
+    @column_names = ["A", "B", "C", "D", "E", "F", "G"]
+    @rows = { 1 => [".", ".", ".", ".", ".", ".", "."],
+              2 => [".", ".", ".", ".", ".", ".", "."],
+              3 => [".", ".", ".", ".", ".", ".", "."],
+              4 => [".", ".", ".", ".", ".", ".", "."],
+              5 => [".", ".", ".", ".", ".", ".", "."],
+              6 => [".", ".", ".", ".", ".", ".", "."]
+            }
+  end
 
-  end 
-
+  def print_board
+    puts @column_names.join
+    @rows.each do |row, spots|
+      puts spots.join + "\n"
+    end
+  end
 
 end
