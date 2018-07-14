29c7c0f6786ac986372530a5c95aca9b224ad771
Kathleen Yruegas <kathleenecn@gmail.com>
2018-07-14 13:03:04 -0600

    Adds welcome message and board printing

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,2 +1,6 @@
+require './lib/board'
+
 puts "Welcome to Connect Four!  Here is your board:"
-# Here, need to create a new board and print that board. 
+# Here, need to create a new board and print that board.
+board = Board.new
+board.print_board
