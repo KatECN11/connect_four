72527207cf40c88520e0608cb59f1af0842d9e37
Kathleen Yruegas <kathleenecn@gmail.com>
2018-07-14 12:34:14 -0600

    Adds test for printing board

--- test/board_test.rb
+++ test/board_test.rb
@@ -9,6 +9,28 @@ class BoardTest < Minitest::Test
     board = Board.new
 
     assert_instance_of Board, board
-  end 
+  end
+
+  def test_it_starts_with_column_headers
+    board = Board.new
+
+    expected = ["A", "B", "C", "D", "E", "F", "G"]
+
+    assert_equal expected, board.column_names
+  end
+
+  def test_it_prints_a_new_board
+    board = Board.new
+
+    expected = "ABCDEFG\n
+                .......\n
+                .......\n
+                .......\n
+                .......\n
+                .......\n
+                ......."
+
+    assert_equal expected, board.print_board
+  end
 
 end
