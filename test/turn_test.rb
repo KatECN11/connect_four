6847d5a1c731b8b6e9d279251984d3e34f36fafd
Kathleen Yruegas <kathleenecn@gmail.com>
2018-07-14 15:19:19 -0600

    Adds test it starts at players turn

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -7,10 +7,15 @@ require './lib/turn'
 class TurnTest < Minitest::Test
 
   def test_it_exists
-    turn = Turn.new("Player")
+    turn = Turn.new
 
     assert_instance_of Turn, turn
-  end 
+  end
 
+  def test_it_starts_with_players_turn
+    turn = Turn.new
+
+    assert_equal "Player", turn.owner
+  end 
 
 end
