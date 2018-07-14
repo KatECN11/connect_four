aad84c3a8757c1cf7e9b1899ccdc037eb448f4de
Kathleen Yruegas <kathleenecn@gmail.com>
2018-07-14 13:16:33 -0600

    Adds boarding printing to runner file

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -3,4 +3,4 @@ require './lib/board'
 puts "Welcome to Connect Four!  Here is your board:"
 # Here, need to create a new board and print that board.
 board = Board.new
-board.print_board
+puts board.print_board
