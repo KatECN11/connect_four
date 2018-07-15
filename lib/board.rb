588329e5666d63dbe69cd5f68aaeae7de0179854
Kathleen Yruegas <kathleenecn@gmail.com>
2018-07-15 10:25:10 -0600

    Refactors print_board method

--- lib/board.rb
+++ lib/board.rb
@@ -14,22 +14,10 @@ class Board
   end
 
   def print_board
-    row_1 = ""
-    row_2 = ""
-    row_3 = ""
-    row_4 = ""
-    row_5 = ""
-    row_6 = ""
-    @columns.values.each do |spots|
-      row_1 += spots[0]
-      row_2 += spots[1]
-      row_3 += spots[2]
-      row_4 += spots[3]
-      row_5 += spots[4]
-      row_6 += spots[5]
-    end
-    printed_board = [@columns.keys.join, row_6, row_5, row_4, row_3, row_2, row_1]
-    return printed_board.join("\n")
+    rows = @columns.values.transpose
+    joint_rows = rows.map do |row|
+                  row.join
+                end
+    @columns.keys.join + ("\n") + joint_rows.join("\n")
   end
-
 end
