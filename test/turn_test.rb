e55f3b0a52c8e9ba6c1e19495a90170f618795ff
Kathleen Yruegas <kathleenecn@gmail.com>
2018-07-15 11:02:30 -0600

    Corrects test for finding next spot

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -7,17 +7,41 @@ require './lib/turn'
 class TurnTest < Minitest::Test
 
   def test_it_exists
-    turn = Turn.new("A")
+    piece = Piece.new("Player", "X")
+    board = Board.new
+    turn = Turn.new("A", board, piece)
 
     assert_instance_of Turn, turn
   end
 
-  def test_it_takes_player_input
-    turn = Turn.new("a")
+  def test_it_takes_player_input_upcased
+    piece = Piece.new("Player", "X")
+    board = Board.new
+    turn = Turn.new("a", board, piece)
 
     assert_equal "A", turn.guess
   end
 
+  def test_it_starts_with_an_empty_board
+    piece = Piece.new("Player", "X")
+    board = Board.new
+    turn = Turn.new("A", board, piece)
+
+    assert_instance_of Board, turn.board
+  end
+
+  def test_it_finds_next_spot_in_column
+    piece = Piece.new("Player", "X")
+    board = Board.new
+    turn = Turn.new("A", board, piece)
+
+    turn.find_spot
+
+    actual = board.columns["A"]
+    expected = ["X", ".", ".", ".", ".", "."]
+
+    assert_equal expected, actual
+  end
 
 
 
