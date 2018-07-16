eb632fb3f5da969507eef1ea7c705626b373d363
Kathleen Yruegas <kathleenecn@gmail.com>
2018-07-16 09:23:18 -0600

    Adds loop for turn

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -2,19 +2,21 @@ require './lib/board'
 require './lib/piece'
 require './lib/turn'
 
-puts "Welcome to Connect Four!  Here is your board:"
-
-# Need to put this into a new Game class so it's not
-# starting a new board everytime
-
-
 board = Board.new
 piece = Piece.new("Player", "X")
 play = Turn.new(board, piece)
 
+puts "Welcome to Connect Four!  Here is your board:"
 board.print_board
 
-puts "What column would you like to place your piece?"
+while play.turn_count <= 7
+  puts "What column would you like to place your piece?"
+  new_guess = gets.chomp
+  play.new_guess(new_guess)
+end
 
-new_guess = gets.chomp
-play.new_guess(new_guess)
+until board.win?
+    puts "What column would you like to place your piece?"
+    new_guess = gets.chomp
+    play.new_guess(new_guess)
+end
