ae7145a0f0b37dc29428763fd3e65bb93ae943b6
Kathleen Yruegas <kathleenecn@gmail.com>
2018-07-17 16:20:33 -0600

    Removes win determination from board class

--- lib/board.rb
+++ lib/board.rb
@@ -24,45 +24,4 @@ class Board
                 end
     @columns.keys.join + ("\n") + joint_rows.reverse.join("\n")
   end
-
-  def four_in_a_column
-    winning_piece = ""
-    @columns.values.each do |spaces|
-        3.times do |i|
-          test = spaces[i]
-          if test != "."
-            if (test == spaces[i+1]) && (test = spaces[i+2]) && (test = spaces[i+3])
-              winning_piece = test
-              break
-            end
-          end
-        end
-      end
-    winning_piece
-  end
-
-  def four_in_a_row
-    winning_piece = ""
-    @columns.values.transpose.each do |spaces|
-        4.times do |i|
-          test = spaces[i]
-          if test != "."
-            if (test == spaces[i+1]) && (test = spaces[i+2]) && (test = spaces[i+3])
-              winning_piece = test
-              break
-            end
-          end
-        end
-      end
-    winning_piece
-  end
-
-  def end?
-    if four_in_a_row == "" && four_in_a_column == ""
-      false
-    else
-      true
-    end
-  end
-
 end
